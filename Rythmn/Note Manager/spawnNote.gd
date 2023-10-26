c6fe92d20a734367f9c8ca21e0bd1d7598a90bf8
Evan Pompa <55860113+PompaEvanB@users.noreply.github.com>
2023-10-26 01:11:08 -0700

    made notes spawn with timer

--- Rythmn/Note Manager/spawnNote.gd
+++ Rythmn/Note Manager/spawnNote.gd
@@ -1,11 +1,40 @@
 extends Node2D
 var note = preload("res://Rythmn/Notes/note.tscn")
+# preload the note assete
+
+# set spawnpoint positions for the notes
+var left = Vector2(0,324);
+var right = Vector2(1152,324);
+var up = Vector2(576,0); 
+var down = Vector2(576,648);
+
+#hold a wait time for each note
+var waitTime = 0;
+
+# this is an array where each note is stored.
+# position, universe color, wait time, 
+var songNotes = [["down","R",1],["left","R",1]]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var instance = note.instantiate()
-	add_child(instance)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	
+	for x in songNotes:
+		var instance = note.instantiate()
+		for y in range(x.size()):
+			# set position of note
+			if y == 0:
+				if x[y] == "left":
+					instance.position = left;
+				if x[y] == "right":
+					instance.position = right;
+				if x[y] == "up":
+					instance.position = up;
+				if x[y] == "down":
+					instance.position = down;
+			# set color of note (not implemented)
+			if y == 1:
+				pass
+			# make the code wait before spawining the note
+			if y == 2:
+				await get_tree().create_timer(x[y]).timeout
+				add_child(instance)
