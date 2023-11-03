c15b45ac40a0cc5e8abcb4f2e6f99bbeac4b1ba6
Kaden-SL <ksedmakl@ucsc.edu>
2023-11-03 16:21:18 -0700

    Pocket Dimension Refs Updated

--- 01 Final Draft/Scripts/Pocket Dimension/Pocket Dimension.gd
+++ 01 Final Draft/Scripts/Pocket Dimension/Pocket Dimension.gd
@@ -3,10 +3,10 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Bbg.visible = false
-	$Gbg.visible = false
-	$Pbg.visible = false
-	$RBG.visible = true
+	$Realism.visible = false
+	$WaterColor.visible = false
+	$Punk.visible = false
+	$EDM.visible = true
 	#bool inR = true
 	#bool inG = false
 	#bool inB = false
@@ -15,28 +15,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_just_pressed("R") && $RBG.visible == false:
-		$RBG.visible = true
-		$Bbg.visible = false
-		$Gbg.visible = false
-		$Pbg.visible = false
+	if Input.is_action_just_pressed("R") && $EDM.visible == false:
+		$EDM.visible = true
+		$Realism.visible = false
+		$WaterColor.visible = false
+		$Punk.visible = false
 		#$Player.play("R")
-	if Input.is_action_just_pressed("B") && $Bbg.visible == false:
-		$Bbg.visible = true
-		$Gbg.visible = false
-		$RBG.visible = false
-		$Pbg.visible = false
+	if Input.is_action_just_pressed("B") && $Realism.visible == false:
+		$Realism.visible = true
+		$WaterColor.visible = false
+		$EDM.visible = false
+		$Punk.visible = false
 		#$Player.play("B")
-	if Input.is_action_just_pressed("G") && $Gbg.visible == false:
-		$Gbg.visible = true
-		$Bbg.visible = false
-		$RBG.visible = false
-		$Pbg.visible = false
+	if Input.is_action_just_pressed("G") && $WaterColor.visible == false:
+		$WaterColor.visible = true
+		$Realism.visible = false
+		$EDM.visible = false
+		$Punk.visible = false
 		#$Player.play("G")
-	if Input.is_action_just_pressed("P") && $Pbg.visible == false:
-		$Pbg.visible = true
-		$Bbg.visible = false
-		$RBG.visible = false
-		$Gbg.visible = false
+	if Input.is_action_just_pressed("P") && $Punk.visible == false:
+		$Punk.visible = true
+		$Realism.visible = false
+		$EDM.visible = false
+		$WaterColor.visible = false
 		#$Player.play("P")
 	pass
