bfc18e29100239a6515252ee51ed805aa982cf80
Kaden-SL <ksedmakl@ucsc.edu>
2023-11-03 15:59:51 -0700

    Final File Str

---
+++ 01 Final Draft/Scripts/Character/PlayerMovement.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+var speed = 250
+var playerOrigin_x
+var playerOrigin_y
+var current_universe = Global.current_universe
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	playerOrigin_x = self.position.x
+	playerOrigin_y = self.position.y
+	pass # Replace with function body.
+	
+func _process(_delta):
+	if Input.is_action_just_pressed("R"):
+		$Sprite2D/AnimatedSprite2D.play("1 R")
+		Global.current_universe = "R"
+	if Input.is_action_just_pressed("G"):
+		$Sprite2D/AnimatedSprite2D.play("2 G")
+		Global.current_universe = "G"
+	if Input.is_action_just_pressed("B"):
+		$Sprite2D/AnimatedSprite2D.play("3 B")
+		Global.current_universe = "B"
+	if Input.is_action_just_pressed("P"):
+		$Sprite2D/AnimatedSprite2D.play("4 P")
+		Global.current_universe = "P"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity = Vector2()
+	
+	if Input.is_action_just_pressed("down"):
+		self.position.y += speed
+	if Input.is_action_just_released("down"):
+		self.position.y = playerOrigin_y
+		self.position.x = playerOrigin_x
+	
+	if Input.is_action_just_pressed("up"):
+		self.position.y -= speed
+	if Input.is_action_just_released("up"):
+		self.position.y = playerOrigin_y
+		self.position.x = playerOrigin_x
+	
+	if Input.is_action_just_pressed("left"):
+		self.position.x -= speed
+	if Input.is_action_just_released("left"):
+		self.position.y = playerOrigin_y
+		self.position.x = playerOrigin_x
+	
+	if Input.is_action_just_pressed("right"):
+		self.position.x += speed
+	if Input.is_action_just_released("right"):
+		self.position.y = playerOrigin_y
+		self.position.x = playerOrigin_x
+	pass
