a42a88cc0cc26e592e9bc6899c32f73b8a867a9f
MatthewOropeza <114529622+MatthewOropeza@users.noreply.github.com>
2023-11-06 15:11:24 -0800

    Intro and Tutorial Narrative
    Added Intro and Tutorial Narrative Scenes at start of game.

---
+++ 01 Final Draft/Tutorial Narrative/tutorial.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var tutorial_intro = $AnimationPlayer
+
+func _ready():
+ tutorial_intro.play("Fade In")
+ get_tree().create_timer(12).timeout.connect(fade_out)
+ 
+
+func fade_out():
+ tutorial_intro.play("Fade Out")
+ get_tree().create_timer(3).timeout.connect(start_game)
+ 
+
+func start_game():
+ get_tree().change_scene_to_file("res://01 Final Draft/Scenes/01-Main/Final Draft.tscn")
